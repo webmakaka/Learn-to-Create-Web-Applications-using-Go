88b5d8c11b517e78a83cb4528ea37a710839c267
Marley <marley.nodejs@gmail.com>
2019-03-25 01:33:25 +0300

    lesson127: Galleries update action

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -76,6 +76,36 @@ func (g *Galleries) Edit(w http.ResponseWriter, r *http.Request) {
 	g.EditView.Render(w, vd)
 }
 
+// POST /galleries/:id/update
+func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
+
+	gallery, err := g.galleryByID(w, r)
+
+	if err != nil {
+		return
+	}
+
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Gallery not found", http.StatusNotFound)
+		return
+	}
+
+	var vd views.Data
+	vd.Yield = gallery
+	var form GalleryForm
+
+	if err := parseForm(r, &form); err != nil {
+		log.Println(err)
+		vd.SetAlert(err)
+		g.EditView.Render(w, vd)
+		return
+	}
+	gallery.Title = form.Title
+	fmt.Fprintln(w, gallery)
+	// g.EditView.Render(w, vd)
+}
+
 // POST /galleries/
 func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
 
--- main.go
+++ main.go
@@ -59,6 +59,8 @@ func main() {
 
 	r.HandleFunc("/galleries/{id:[0-9]+}/edit", requireUserMw.ApplyFn(galleriesC.Edit)).Methods("GET")
 
+	r.HandleFunc("/galleries/{id:[0-9]+}/update", requireUserMw.ApplyFn(galleriesC.Update)).Methods("POST")
+
 	http.ListenAndServe(":3000", r)
 }
 
