10e3eb71a12f4b76c1a7785416b0eaea0ad50a75
Marley <marley.nodejs@gmail.com>
2019-03-27 07:02:06 +0300

    lesson133: File upload handler

--- main.go
+++ main.go
@@ -66,6 +66,8 @@ func main() {
 
 	r.HandleFunc("/galleries/{id:[0-9]+}/update", requireUserMw.ApplyFn(galleriesC.Update)).Methods("POST")
 
+	r.HandleFunc("/galleries/{id:[0-9]+}/images", requireUserMw.ApplyFn(galleriesC.ImageUpload)).Methods("POST")
+
 	r.HandleFunc("/galleries/{id:[0-9]+}/delete", requireUserMw.ApplyFn(galleriesC.Delete)).Methods("POST")
 
 	http.ListenAndServe(":3000", userMw.Apply(r))
